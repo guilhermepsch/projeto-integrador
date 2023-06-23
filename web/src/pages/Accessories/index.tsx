3244615e6aca019f4045c2ffb59b9e3acb9931d2
Guilherme Pereira Schneidt <guilhermepsch@gmail.com>
2023-06-23 13:30:28 -0300

    feat: criada pagina de acessorios (#15)

--- web/src/pages/Accessories/index.tsx
+++ web/src/pages/Accessories/index.tsx
@@ -1,3 +1,20 @@
-export default function Acessorios() {
-	return <h1>Acessórios</h1>;
+import AccessoryItem from './AccessoryItem';
+import './styles.css';
+import accessory1 from '../../assets/img/mock/accessory1.png';
+
+export default function Accessories() {
+	return (
+		<>
+			<div className="accessory-title">A C E S S Ó R I O S</div>
+			<div className="accessory-list-container">
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+				<AccessoryItem name="Bolsa" image={accessory1} price={100} />
+			</div>
+		</>
+	);
 }
