973d38e6f37202d435a6a41dfe160b798c63b269
Mariana Lino <marianalinodasilva@gmail.com>
2023-07-05 09:44:15 -0300

    feat: alteracoes feitas na casa do gui

--- web/src/pages/Cart/index.tsx
+++ web/src/pages/Cart/index.tsx
@@ -1,55 +1,114 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import "./styles.css";
-import { Cart, getCartById } from "../../requests/CartRequests";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './styles.css';
+import {
+	Cart,
+	Product,
+	getCartById,
+	getProductById,
+} from '../../requests/CartRequests';
+import { Item, getItemsByCartId } from '../../requests/CartRequests';
 
 export default function Carrinho() {
-  const { id } = useParams();
-  const [cart, setCart] = useState<Cart | null | undefined>(null);
+	const { id } = useParams();
+	const [cart, setCart] = useState<Cart | null | undefined>(null);
+	const [cartItems, setCartItems] = useState<Item[] | null | undefined>(null);
+	const [product, setProduct] = useState<Product | null | undefined>(null);
+	const arrayProduto: number[] = [];
 
-  useEffect(() => {
-    getCartById(Number(id)).then((cart) => {
-      if (cart != null) {
-        setCart(cart);
-      } else {
-        setCart(undefined);
-      }
-    });
-  }, [id]);
+	useEffect(() => {
+		getCartById(Number(id)).then(cart => {
+			if (cart != null) {
+				setCart(cart);
+			} else {
+				setCart(undefined);
+			}
+		});
 
-  return (
-    <>
-      {cart === null ? (
-        <p>Carregando...</p>
-      ) : cart === undefined ? (
-        <>
-          <div className="empty-cart">
-            <p>
-              O seu carrinho está vazio... <br />
-              Volte e dê uma olhada em nosso catálogo!
-            </p>
-          </div>
-          <span onClick={
-            () => window.history.back()
-          } className="back-button">Voltar</span>
-        </>
-      ) : (
-        <>
-        <div className="items-box">
-          <div className="item-quantity">
-            item 1 de 1
-          </div>
-          <div className="item-divider" />
-          <div className="item-image">
-            <img 
-            src=""
-            alt=""
-            height={135}
-            width={135} />
-          </div>
-        </div>
-        </>
-      )}
-    </>
-  );
+		getItemsByCartId(Number(id)).then(cartItems => {
+			if (cartItems != null) {
+				setCartItems(cartItems);
+			} else {
+				setCartItems(undefined);
+			}
+		});
+
+		getProductById(Number(id)).then(product => {
+			if (product != null) {
+				setProduct(product);
+			} else {
+				setProduct(undefined);
+			}
+		});
+
+			// check if cartItems is empty, if it is not, count the quantity of each product
+			 cartItems?.map(item => {
+			 	if (item.prod_id != null) {
+			 		arrayProduto.push(item.prod_id);
+				}
+			});
+
+			//check arrayProduto and count the quantity of each product
+			const count = arrayProduto.reduce((acc: Record<number, number>, curr) => {
+				if (typeof acc[curr] === 'undefined') {
+					acc[curr] = 1;
+				} else {
+					acc[curr] += 1;
+				}
+				return acc;
+			}, {});
+
+			console.log(count);
+
+	}, [id]);
+
+	return (
+		<>
+			{cart === null ? (
+				<p>Carregando...</p>
+			) : cart === undefined ? (
+				<>
+					<div className="empty-cart">
+						<p>
+							O seu carrinho está vazio... <br />
+							Volte e dê uma olhada em nosso catálogo!
+						</p>
+					</div>
+					<span
+						onClick={() => window.history.back()}
+						className="back-button">
+						Voltar
+					</span>
+				</>
+			) : (
+				<>
+					<div className="items-box">
+						<div className="item-quantity">
+							{cartItems && cartItems.length < 2
+								? `${cartItems?.length} item`
+								: `${cartItems?.length} itens`}
+						</div>
+						<div className="item-divider" />
+						{cartItems &&
+							cartItems.map(item => (
+								<>
+									<div className="items-info">
+										<img
+											src="product?.prod_image"
+											alt=""
+											height={135}
+											width={135}
+										/>
+										<div className="item-name">
+											{product?.prod_name}
+										</div>
+									</div>
+									<div className="item-divider" />
+								</>
+							))}
+					</div>
+				</>
+			)}
+		</>
+	);
 }
--- web/src/requests/CartRequests.ts
+++ web/src/requests/CartRequests.ts
@@ -7,6 +7,25 @@ export type Cart = {
   updated_at: string;
 };
 
+export type Item = {
+	item_id: number;
+	cart_id: number;
+	prod_id: number;
+	created_at: string;
+	updated_at: string;
+};
+
+export type Product = {
+	prod_id: number;
+	prod_name: string;
+	prod_price: number;
+	prod_description: string;
+	prod_image: string;
+	cata_id: number;
+	created_at: string;
+	updated_at: string;
+};
+
 export async function getCartById(id: number): Promise<Cart | null> {
 	try {
 		const response = await axios.request({
@@ -18,6 +37,40 @@ export async function getCartById(id: number): Promise<Cart | null> {
 		});
 		const cart: Cart = response.data;
 		return cart;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
+}
+
+export async function getItemsByCartId(cart_id: number){
+	try {
+		const response = await axios.request({
+			method: 'GET',
+			url: 'http://localhost:5000/item/cart/' + cart_id,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const items: Item[] = response.data;
+		return items;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
+}
+
+export async function getProductById(prod_id: number){
+	try {
+		const response = await axios.request({
+			method: 'GET',
+			url: 'http://localhost:5000/product/' + prod_id,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const product: Product = response.data;
+		return product;
 	} catch (err) {
 		console.log(err);
 		return null;
